b08fdb47d33c894c1b6bc575cbc7d5320119987f
Bojan Jakimovski <bokicasheks@yahoo.com>
2024-08-11 03:30:25 +0200

    Added langfuse to the simple rag pipeline

--- src/pipelines/simple_rag_pipeline.py
+++ src/pipelines/simple_rag_pipeline.py
@@ -2,8 +2,12 @@
 This module defines the pipeline execution for SimpleRAG.
 """
 
-from src.models.simple_rag import SimpleRAG
+import os
+
+from langfuse.callback import CallbackHandler
+
 from src.configuration.configuration_model import SimpleRAGConfig
+from src.models.simple_rag import SimpleRAG
 
 
 def simple_rag_pipeline_execution(config: SimpleRAGConfig, prompt: list, questions: list):
@@ -18,6 +22,13 @@ def simple_rag_pipeline_execution(config: SimpleRAGConfig, prompt: list, questio
     Returns:
         None
     """
+    langfuse_handler = CallbackHandler(
+        secret_key=os.environ["LANGFUSE_SECRET_KEY"],
+        public_key=os.environ["LANGFUSE_PUBLIC_KEY"],
+        host=os.environ["LANGFUSE_HOST"],
+        session_id=config.experiment_name,
+    )
+
     simple_rag = SimpleRAG(config)
     simple_rag.initialize_base()
 
@@ -31,5 +42,11 @@ def simple_rag_pipeline_execution(config: SimpleRAGConfig, prompt: list, questio
     simple_rag.initialize_retriever()
     chain = simple_rag.get_retrieval_qa_chain()
 
+    questions = questions[1:2]
     for question in questions:
-        print(chain.invoke(question))
+        contexts = [context.page_content for context in simple_rag.retriever.invoke(question)]
+        answer = chain.invoke(
+            question,
+            config={"callbacks": [langfuse_handler]},
+        )['result']
+        print(question, answer, contexts)
