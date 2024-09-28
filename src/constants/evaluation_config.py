f5f178a3c410d3f10d72dc1334537b137413ca6c
Bojan Jakimovski <bokicasheks@yahoo.com>
2024-09-28 14:44:54 +0200

    Updating the LLM Judge to be stricter and more deterministic and adding one more question for evaluation for the new paper

--- src/constants/evaluation_config.py
+++ src/constants/evaluation_config.py
@@ -4,7 +4,7 @@ from ragas.metrics import answer_relevancy, context_utilization, faithfulness
 
 LLM_MODEL_ID = "anthropic.claude-3-5-sonnet-20240620-v1:0"
 
-LLM_MODEL_KWARGS = {"max_tokens": 4096, "temperature": 0.1}
+LLM_MODEL_KWARGS = {"max_tokens": 4096, "temperature": 0, "top_p": 0}
 
 EMBEDDER_MODEL_ID = "amazon.titan-embed-text-v2:0"
 
